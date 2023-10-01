40d3b4d2da2643d7e34b0aca5d9689362e5f374f
Jessica Chen <jc939@duke.edu>
2023-10-01 19:08:21 -0400

    fixed some stuff

--- pages/social.tsx
+++ pages/social.tsx
@@ -1,8 +1,7 @@
 import Image          from 'next/image';
 import Link           from 'next/link';
 import React, { FC }  from 'react';
-import GlobalLayout   from '@/app/layout'
-import DefLayout      from '@/components/def_layout'
+import DefLayout      from '@/components/def_layout';
 
 const SocialPage: React.FC = () => {
   return (
