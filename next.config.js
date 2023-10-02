a163e3589aae1f9598761216fb39b4ea49fcbe3e
Jessica Chen <jc939@duke.edu>
2023-10-02 11:27:54 -0400

    finished auth0

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '/api/:path*',
+        source: '/api/:path((?!auth).*)',
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5328/api/:path*'
