1bcb0b03843d97124950a3ae37064f50c1ab3bea
Jessica Chen <jc939@duke.edu>
2023-10-01 20:27:49 -0400

    auth0 stuff

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '@/app/globals.css'
+import { UserProvider } from "@auth0/nextjs-auth0/client"
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
+}
