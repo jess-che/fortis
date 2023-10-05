1e56fbba1e6c6833aecece45efabb8091caad75f
Austin Huang <ahumblehuang223@gmail.com>
2023-10-05 00:38:31 -0400

    deleted placeholder fetch

--- pages/SearchBarComponents/SearchBar.tsx
+++ pages/SearchBarComponents/SearchBar.tsx
@@ -9,23 +9,6 @@ type User = {
 const SearchBar: React.FC = () => {
   const [input, setInput] = useState("");
 
-  const fetchData = (value: string) => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((json) => {
-        const results = json.filter((user: User) => {
-          return (
-            value &&
-            user &&
-            user.name &&
-            user.name.toLowerCase().includes(value.toLowerCase())
-          );
-        });
-
-        console.log(results);
-      });
-  };
-
   const saveSearchQueryToDatabase = async (query: string) => {
     const response = await fetch('/api/searchExcName', {
       method: 'POST',
@@ -46,7 +29,6 @@ const SearchBar: React.FC = () => {
 
   const handleChange = (value: string) => {
     setInput(value);
-    fetchData(value);
     saveSearchQueryToDatabase(value);
   };
 
@@ -62,64 +44,3 @@ const SearchBar: React.FC = () => {
 };
 
 export default SearchBar;
-
-
-
-// import React, { useState, FC } from "react";
-
-// // import { FaSearch } from "react-icons/fa";
-// import "./SearchBar.css";
-
-// // Define a type for the user objects
-// type User = {
-//   id: number;
-//   name: string;
-//   // Add other properties as needed
-// };
-
-// // type SearchBarProps = {
-// //     setResults: React.Dispatch<React.SetStateAction<User[]>>;
-// //   };
-  
-// const SearchBar: React.FC = () => {
-//   const [input, setInput] = useState("");
-
-//   const fetchData = (value: string) => {
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//       .then((response) => response.json())
-//       .then((json) => {
-//         const results = json.filter((user: User) => {
-//           return (
-//             value &&
-//             user &&
-//             user.name &&
-//             user.name.toLowerCase().includes(value)
-//           );
-//         });
-//         // setResults(results);
-//       });
-//   };
-
-//   const handleChange = (value: string) => {
-//     setInput(value);
-//     fetchData(value);
-//   };
-
-//   return (
-//     <div className="input-wrapper">
-//       {/* <FaSearch id="search-icon" /> */}
-//       <input
-//         placeholder="Type to search..."
-//         value={input}
-//         onChange={(e) => handleChange(e.target.value)}
-//       />
-//     </div>
-//   );
-// };
-
-// export default SearchBar;
-
-
-// // type SearchBarProps = {
-// //     setResults: React.Dispatch<React.SetStateAction<User[]>>; // Assuming results is an array of User objects
-// //   };
