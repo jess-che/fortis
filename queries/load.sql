bb769b656fd2377421990d8e53fe9c1a4f85ef3f
Austin Huang <ahumblehuang223@gmail.com>
2023-10-04 23:55:12 -0400

    populating exercises table sql

---
+++ queries/load.sql
@@ -0,0 +1,8 @@
+INSERT INTO exercise (eid, name, type, muscle_group, popularity, equipment, favorite)
+VALUES
+    (1, 'Push-up', 'Bodyweight', 'Chest', 100, 'None', true),
+    (2, 'Bench Press', 'Strength', 'Chest', 90, 'Barbell', false),
+    (3, 'Squat', 'Strength', 'Legs', 95, 'Barbell', true),
+    (4, 'Deadlift', 'Strength', 'Back', 90, 'Barbell', false),
+    (5, 'Pull-up', 'Bodyweight', 'Back', 85, 'None', true),
+    (6, 'Plank', 'Core', 'Abs', 80, 'None', false);
