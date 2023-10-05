624d5f7a1e78251f33bfd5c6afca7e908d3739cf
Benjamin Chauhan <benjamin.chauhan@duke.edu>
2023-10-05 14:32:08 -0400

    add email search to social page

--- pages/SocialSearchBarComponents/SearchBar.tsx
+++ pages/SocialSearchBarComponents/SearchBar.tsx
@@ -1,55 +1,77 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import SearchResultsList from "./SearchResultsList";
 import "./SearchBar.css";
 
 
 const SearchBar = () => {
-  const [input, setInput] = useState("");
-  const [results, setResults] = useState([]);
+    const [input, setInput] = useState("");
+    const [results, setResults] = useState([]);
 
-  const populatelist = async (query: any) => {
-    const response = await fetch('/api/searchUserName', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        searchQuery: query
-      }),
-    });
+    const populatelist = async (query: any) => {
+        const response = await fetch('/api/searchUserName', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                searchQuery: query
+            }),
+        });
 
-    if (!response.ok) {
-      throw new Error('Failed to save query');
-    }
+        if (!response.ok) {
+            throw new Error('Failed to save query');
+        }
 
-    const data = await response.json();
-    const dataName = data.data.rows.map((row: { name: any; }) => row.name);
-    setResults(dataName);
-    console.log(dataName)
-  };
+        const data = await response.json();
+        const dataName = data.data.rows.map((row: { name: any; }) => row.name);
+        setResults(dataName);
+        console.log(dataName)
+    };
 
-  // so names load on start
-  useEffect(() => {
-    handleChange(''); 
-  }, []); 
+    const populatelist2 = async (query: any) => {
+        const response = await fetch('/api/searchUserEmail', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                searchQuery: query
+            }),
+        });
 
-  const handleChange = (value: React.SetStateAction<string>) => {
-    setInput(value);
-    populatelist(value);
-  };
+        if (!response.ok) {
+            throw new Error('Failed to save query');
+        }
 
-  return (
-    <div className="search-container">
-      <div className="input-wrapper">
-        <input
-          placeholder="Type to search..."
-          value={input}
-          onChange={(e) => handleChange(e.target.value)}
-        />
-      </div>
-      {results && results.length > 0 && <SearchResultsList results={results} />}
-    </div>
-  );
+        const data = await response.json();
+        const dataName = data.data.rows.map((row: { name: any; }) => row.name);
+        setResults(prevResults => [...prevResults, ...dataName]);
+        console.log(dataName)
+    };
+
+
+    // so names load on start
+    useEffect(() => {
+        handleChange('');
+    }, []);
+
+    const handleChange = (value: React.SetStateAction<string>) => {
+        setInput(value);
+        populatelist(value);
+    };
+
+    return (
+        <div className="search-container">
+            <div className="input-wrapper">
+                <input
+                    placeholder="Type to search..."
+                    value={input}
+                    onChange={(e) => handleChange(e.target.value)}
+                />
+            </div>
+            {results && results.length > 0 && <SearchResultsList results={results}/>}
+        </div>
+    );
 };
 
 export default SearchBar;
