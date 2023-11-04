a8f71c98f910d8725bad39f62fd0a4a9d3921927
Swarajh Mehta <swarajhmehta1@gmail.com>
2023-11-04 19:41:54 -0400

    depl check

--- pages/MuscleModel.js
+++ pages/MuscleModel.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './MuscleModel.css';
 
 const MuscleModel = ({ muscleGroups }) => {
+  if (!muscleGroups || muscleGroups.length === 0) {
+    return <p>No muscle group data available.</p>;
+  }
+
   return (
     <div className="muscle-model-container">
       {muscleGroups.map((group, index) => (
