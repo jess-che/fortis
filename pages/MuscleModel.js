536a874b9b7452e512db3d2ab52f2ab6a76b4c2e
Swarajh Mehta <swarajhmehta1@gmail.com>
2023-11-04 19:47:30 -0400

    colour modulation on bobboxes

--- pages/MuscleModel.js
+++ pages/MuscleModel.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './MuscleModel.css';
 
+const MAX_SETS = 20; // Maximum number of sets for full color intensity
+
 const MuscleModel = ({ muscleGroups }) => {
   if (!muscleGroups || muscleGroups.length === 0) {
     return <p>No muscle group data available.</p>;
   }
 
+  const getColorForSets = (sets) => {
+    const intensity = Math.min(sets / MAX_SETS, 1);
+    const colorIntensity = Math.floor(255 * intensity);
+    return `rgb(${colorIntensity}, 0, 0)`;
+  };
+
   return (
     <div className="muscle-model-container">
       {muscleGroups.map((group, index) => (
-        <div key={index} className="muscle-group-box">
+        <div 
+          key={index} 
+          className="muscle-group-box" 
+          style={{ backgroundColor: getColorForSets(group.sets) }}
+        >
           <h3>{group.muscle_group}</h3>
           <p>Sets: {group.sets}</p>
         </div>
