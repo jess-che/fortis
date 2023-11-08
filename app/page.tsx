358018ff9968708eac5e6c8a2dd3ca4e033289af
Jessica Chen <jc939@duke.edu>
2023-11-08 11:01:39 -0500

    trying out cookies

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,49 @@
 'use client'
-import Image          from 'next/image';
-import Link           from 'next/link';
-import React, { FC }  from 'react';
-import DefLayout      from '@/components/def_layout';
-import LoginLayout    from '@/components/login_layout';
-import { useUser }    from '@auth0/nextjs-auth0/client';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { FC } from 'react';
+import DefLayout from '@/components/def_layout';
+import LoginLayout from '@/components/login_layout';
+import { useUser } from '@auth0/nextjs-auth0/client';
 import { useState, useEffect } from 'react';
 import '@/public/styles/home.css';                              // style sheet for animations
 
+document.cookie = "uid=b24e24f4-86b8-4b83-8947-b2472a43b436"
 
 const Home: React.FC = () => {
   const { user, error, isLoading } = useUser();                 // auth0 login status
   let firstLogin = false;                                       // check if first time logged in
-  
+
   // states to do scrolling information effect
-  const [showTextBox, setShowTextBox] = useState(false);        
+  const [showTextBox, setShowTextBox] = useState(false);
   const [showScrollArrow, setShowScrollArrow] = useState(true);
+
+  const getUID = async (userEmail: any) => {
+    try {
+      const response = await fetch('/api/GetUIDfromEmail', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          searchQuery: userEmail,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to get UID from email');
+      }
+
+      const data = await response.json();
+      document.cookie = data.data.rows[0].uid;
+      console.log("Got UID in History: ");
+      console.group(document.cookie);
+      return data.data.rows[0].uid;
+    }
+    catch {
+
+    }
+  };
 
   useEffect(() => {
     // This function will be called on scroll events
@@ -28,21 +56,21 @@ const Home: React.FC = () => {
         setShowScrollArrow(true);
       }
     };
-  
+
     // Add the scroll event listener
     window.addEventListener('scroll', handleScroll);
-  
+
     // Call AnalStreaks with the user's email if the user is logged in
     if (user && !isLoading && !error) {
       AnalStreaks({ email: user.email }).catch(console.error);
     }
-  
+
     // Cleanup function to remove the event listener when the component unmounts
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, [user, isLoading, error]); // Dependencies for useEffect
-  
+
   const AnalStreaks = async (query: any) => {
     const response = await fetch('/api/AnalStreaks', {
       method: 'POST',
@@ -74,12 +102,12 @@ const Home: React.FC = () => {
         email: user.email
       }),
     });
-    
+
     if (!response.ok) {
-        throw new Error('Failed to save user');
-      }
+      throw new Error('Failed to save user');
+    }
   };
-  
+
   // function to call to access saving user to database
   const handleUserSave = async () => {
     if (!user) {
@@ -98,49 +126,49 @@ const Home: React.FC = () => {
   // home if no one is logged in
   if (!user) {
     return (
-        // login layout so we get the login effect
-        <LoginLayout>
-          <div className="flex flex-col w-screen min-h-[90vh] mb-5 justify-center items-center">
-            {/* default center */}
-            <Image
-              src="/animated/pulseLogo.svg"
-              alt="Pulsing Logo"
-              width={150}
-              height={150}
-            />
-            
-            <div className="text-7xl font-bold glow-text text-center">Fort&iacute;s</div>
+      // login layout so we get the login effect
+      <LoginLayout>
+        <div className="flex flex-col w-screen min-h-[90vh] mb-5 justify-center items-center">
+          {/* default center */}
+          <Image
+            src="/animated/pulseLogo.svg"
+            alt="Pulsing Logo"
+            width={150}
+            height={150}
+          />
 
-            <div className="mt-7 relative"> 
-              <Link href="/api/auth/login">
-                <button className="startNow rounded-md px-2 text-xl font-bold inset-0 text-center transition hover:bg-transparent border-2 border-[#55BBA4]">
-                  Start Now
-                </button>
-              </Link>
-            </div>
-            {/* default center */}
+          <div className="text-7xl font-bold glow-text text-center">Fort&iacute;s</div>
 
-            {/* scrolling effects */}
-            {/* animation for . .. */}
-            <style jsx>{`
+          <div className="mt-7 relative">
+            <Link href="/api/auth/login">
+              <button className="startNow rounded-md px-2 text-xl font-bold inset-0 text-center transition hover:bg-transparent border-2 border-[#55BBA4]">
+                Start Now
+              </button>
+            </Link>
+          </div>
+          {/* default center */}
+
+          {/* scrolling effects */}
+          {/* animation for . .. */}
+          <style jsx>{`
               .ellipsis::after {
                 content: ".";
                 animation: ellipsisAnimation 3s ease-in infinite;
               }
             `}</style>
 
-            {/* hides when scroll */}
-            <div className={`${showScrollArrow ? 'block' : 'hidden'} mt-5 opacity-75`}>
-               <div className="text-sm ellipsis">Scroll down</div>
-            </div>
-
-            {/* appears when scroll */}
-            <div className={`${showTextBox ? 'opacity-100 visible' : 'opacity-0 invisible'} transition-all duration-500 mt-5 text-white text-center w-[45vw]`}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat rem quidem inventore consequatur quis doloribus perspiciatis illum nam dicta tempora facilis, corporis atque. Quos beatae, laudantium dicta cupiditate pariatur doloremque?
-            </div>
-            {/* scrolling effects */}
+          {/* hides when scroll */}
+          <div className={`${showScrollArrow ? 'block' : 'hidden'} mt-5 opacity-75`}>
+            <div className="text-sm ellipsis">Scroll down</div>
           </div>
-        </LoginLayout>
+
+          {/* appears when scroll */}
+          <div className={`${showTextBox ? 'opacity-100 visible' : 'opacity-0 invisible'} transition-all duration-500 mt-5 text-white text-center w-[45vw]`}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat rem quidem inventore consequatur quis doloribus perspiciatis illum nam dicta tempora facilis, corporis atque. Quos beatae, laudantium dicta cupiditate pariatur doloremque?
+          </div>
+          {/* scrolling effects */}
+        </div>
+      </LoginLayout>
     );
   }
 
@@ -148,22 +176,27 @@ const Home: React.FC = () => {
   if (user && user['https://cs316-fortis.vercel.app/firstLogin']) {
     firstLogin = user['https://cs316-fortis.vercel.app/firstLogin'] as boolean;
   }
+
   // if it is, handle th esave
   if (firstLogin) {
     handleUserSave();
   }
 
-  // since you got here, you are logged in -- see this
-  return (
-    <DefLayout>
-      
-      <div>
-        Welcome, {user.name}. This is home page.
-        Your email is {user.email}. 
-      </div>
-    
-    </DefLayout>
-  );
+  if (user) {
+    getUID(user.email);
+
+    // since you got here, you are logged in -- see this
+    return (
+      <DefLayout>
+
+        <div>
+          Welcome, {user.name}. This is home page.
+          Your email is {user.email}.
+        </div>
+
+      </DefLayout>
+    );
+  }
 }
 
 export default Home;
