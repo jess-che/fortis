c508a6abbd655d50a95154cb485b9ecfef4f8388
Jessica Chen <jc939@duke.edu>
2023-11-29 11:30:20 -0500

    working on logfinish/edit

---
+++ pages/api/getActivities.js
@@ -0,0 +1,40 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    // connectionString: process.env.POSTGRES_URL
+    connectionString: "postgres://default:Azy2srgWb9aU@ep-polished-cherry-55480419-pooler.us-east-1.postgres.vercel-storage.com/verceldb?sslmode=require"
+});
+
+const History = `
+    SELECT 
+        *, 
+        to_char(activity."Date", 'YYYY-MM-DD') AS formatted_date
+    FROM 
+        activity
+    WHERE 
+        activity."Uid" = $1 
+        AND activity."Aid" = $2
+`;
+
+
+export default async (req, res) => {
+    if (req.method === 'POST') {
+        const searchQuery = req.body.searchQuery;
+        const aid = req.body.aid;
+
+        try {
+            // Insert user
+            const values = [`${searchQuery}`, aid];
+            console.log('Success! HistoryActivities');
+            const results = await pool.query(History, values);
+
+            res.json({ success: true, data: results });
+        } catch (err) {
+            console.log('Error in HistoryActivites');
+            console.error(err);
+            res.status(500).json({ success: false, message: 'Internal Server Error' });
+        }
+    } else {
+        res.status(405).end();  // Method Not Allowed
+    }
+};
