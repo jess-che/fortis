ecc44009e808169850089c12954caa3e6237c9ec
Jessica Chen <jc939@duke.edu>
2023-11-30 00:46:39 -0500

    oh is it the quotation mark again

--- app/page.tsx
+++ app/page.tsx
@@ -114,8 +114,8 @@ const Home: React.FC = () => {
 
       const data = await response.json();
       console.log(getCookie('name'))
-      setCookie('name', data.data.rows[0].uid);
-      return data.data.rows[0].uid;
+      setCookie('name', data.data.rows[0].name);
+      return data.data.rows[0].name;
     }
     catch (err){
       console.log('Error in GetUIDfromEmail:', err);
