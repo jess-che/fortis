d331d52e22e926502177a28c2644da053cc76c82
Swarajh Mehta <swarajhmehta1@gmail.com>
2023-12-01 02:14:11 -0500

    finished matcher for now

--- pages/social.tsx
+++ pages/social.tsx
@@ -31,6 +31,8 @@ interface Person {
   location: string;
   workoutTypes: string;
   softPreferences: string;
+  frequency: string;
+  gymAvailability: string;
   // Add other fields as needed
 }
 
@@ -39,11 +41,27 @@ interface MatchedPersonDisplayProps {
 }
 
 const MatchedPersonDisplay: React.FC<MatchedPersonDisplayProps> = ({ person }) => {
+  // Function to parse frequency and workout types to a more readable format
+  const parseList = (listString: string) => {
+    return listString.replace(/[{}"]/g, '').split(',').join(', ');
+  };
+
+  // Function to parse gym availability to a more readable format
+  const parseGymAvailability = (availabilityString: string) => {
+    const times = ["6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+    return availabilityString.replace(/[{}"]/g, '').split(',')
+      .map((available, index) => available === 'true' ? times[index] : null)
+      .filter(time => time !== null)
+      .join(', ');
+  };
+
   return (
     <div className={styles.matchedPerson}>
       <p>Name: {person.name || 'Not specified'}</p>
       <p>Location: {person.location}</p>
-      <p>Workout Types: {person.workoutTypes}</p>
+      <p>Frequency: {parseList(person.frequency)}</p>
+      <p>Gym availability: {parseGymAvailability(person.gymAvailability)}</p>
+      <p>Workout Types: {parseList(person.workoutTypes)}</p>
       <p>Soft Preferences: {person.softPreferences}</p>
       {/* Add more fields as needed */}
     </div>
