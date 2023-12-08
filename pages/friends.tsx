0c2d0c89fa76fc216bfbb5550344ab9ae205aa39
Jessica Chen <jc939@duke.edu>
2023-12-08 03:37:45 -0500

    finished friends

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -125,15 +125,22 @@ const FlexiblePersonList1: React.FC<FlexiblePersonListProps> = ({ people, onAcce
 
   return (
     <ul className="">
-    {people.map((person, index) => (
-      <li key={person.uid} className={`border border-white p-3 rounded-md m-3 border-opacity-60 bg-white bg-opacity-5 ${isFirstPersonActive && index === 0 ? 'bg-red-900' : ''}`}>
+      {people.map((person, index) => (
+        <li key={person.uid} className={`border border-white p-3 rounded-md m-3 border-opacity-60 bg-white bg-opacity-5 ${isFirstPersonActive ? 'glow-effect ' : ''}`}>
           <div className='flex flex-row items-center justify-between'>
             <h2 className="text-2xl font-bold  gradient-text-pb">{person.name || 'Not specified'}</h2>
-            <button onClick={() => handleFriendRequest(person.uid)} className="">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-              </svg>
-            </button>
+            <div className="flex flex-row items-center justify-center">
+              {isFirstPersonActive ?
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#C32E67" className="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 013 3h-15a3 3 0 013-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 01-.982-3.172M9.497 14.25a7.454 7.454 0 00.981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 007.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 002.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 012.916.52 6.003 6.003 0 01-5.395 4.972m0 0a6.726 6.726 0 01-2.749 1.35m0 0a6.772 6.772 0 01-3.044 0" />
+                </svg>
+                : ''}
+              <button onClick={() => handleFriendRequest(person.uid)} className="">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="ml-2 w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                </svg>
+              </button>
+            </div>
           </div>
 
           <div className='grid grid-cols-5 gap-1'>
@@ -146,7 +153,7 @@ const FlexiblePersonList1: React.FC<FlexiblePersonListProps> = ({ people, onAcce
             <p className="col-span-1 font-bold">Email:</p>
             <p className="col-span-4">{person.email !== null ? person.email : ''}</p>
 
-            <p className="col-span-1 font-bold">Sets (2 weeks):</p>
+            <p className="col-span-1 font-bold">Sets <br /> (2w):</p>
             <p className="col-span-4">{person.totalsets !== 0 ? person.totalsets : 'FAILURE'}</p>
 
             <p className="col-span-1 font-bold">About:</p>
