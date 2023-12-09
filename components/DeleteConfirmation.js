5ab2e48498e4fbe50c8fe571cda707d0fbe244d0
Jessica Chen <jc939@duke.edu>
2023-12-09 13:28:14 -0500

    code cleaned two component codes

--- components/DeleteConfirmation.js
+++ components/DeleteConfirmation.js
@@ -1,19 +1,25 @@
-// DeleteConfirmation.js
-
+// confirmation for deleting account -> profile
 import React from 'react';
 
 const DeleteConfirmation = ({ onCancel, onConfirm }) => {
   return (
+    // size of screen, make everything else untouchable
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+      {/* focus message */}
       <div className="bg-[#121212] backdrop-blur-md bg-opacity-70 border-2 border-white border-opacity-30 p-6 rounded-lg shadow-lg">
+        {/* warning text */}
         <p className="text-lg font-semibold mb-4">Are you sure you want to delete your account?</p>
+
         <div className="flex justify-end">
+          {/* cancel delete */}
           <button
             onClick={onCancel}
             className="mr-2 px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400"
           >
             Cancel
           </button>
+
+          {/* delete account */}
           <button
             onClick={onConfirm}
             className="px-4 py-2 bg-[#C32E67] text-white rounded hover:bg-opacity-80"
--- components/PrivacyOptionsDialog.js
+++ components/PrivacyOptionsDialog.js
@@ -1,23 +1,31 @@
+// privacy choice for saving template account -> logfinish
 import React, { useState } from 'react';
 
 const PrivacyOptionsDialog = ({ onClose, onSave }) => {
-  const [selectedOption, setSelectedOption] = useState(null); // Initialize to null
+  // keeps track of which privacy mode they have selected
+  const [selectedOption, setSelectedOption] = useState(null);  
 
+  // function to handle changing choice
   const handleOptionChange = (option) => {
     console.log("Option selected:", option);
     setSelectedOption(option);
   };
   
-
+  // function to handle final selection
   const handleSave = () => {
     onSave(selectedOption);
     onClose();
   };
 
   return (
+    // privacy box make it pop up
     <div className="flex items-end justify-center z-50">
+      {/* actual visible component */}
       <div className="p-4 bg-[#121212] bg-opacity-20 rounded-lg shadow-md backdrop-blur-md border border-white border-opacity-40">
+        {/* dialog */}
         <h2 className="text-lg font-semibold mb-2">Select Privacy Option:</h2>
+
+        {/* options */}
         <div className="space-y-2 flex flex-col">
           <label>
             <input
@@ -47,13 +55,17 @@ const PrivacyOptionsDialog = ({ onClose, onSave }) => {
             Public
           </label>
         </div>
+
+        {/* button to save or cancel */}
         <div className="flex justify-end mt-1">
+          {/* save saves final selected choice */}
           <button
             className="px-4 py-2 text-white bg-[#2FABDD] rounded opacity-90 hover:opacity-80"
             onClick={handleSave}
           >
             Save
           </button>
+          {/* cancel closes the dialog */}
           <button
             className="px-4 py-2 ml-2 text-white hover:text-opacity-90"
             onClick={onClose}
