2e7cf4f4e6e61f9f552096d3110b76d4948f7eef
Swarajh Mehta <swarajhmehta1@gmail.com>
2023-12-08 23:56:55 -0500

    relevance and search privacy

--- pages/api/searchUserName.js
+++ pages/api/searchUserName.js
@@ -5,14 +5,23 @@ const pool = new Pool({
     connectionString: "postgres://default:Azy2srgWb9aU@ep-polished-cherry-55480419-pooler.us-east-1.postgres.vercel-storage.com/verceldb?sslmode=require"
 });
 
+// I am using a relevance score to measure if the search is start of name vs. end of name.
 const searchUserName = `
-    SELECT * FROM user_data
-    WHERE name LIKE $1 AND uid <> $2
+    SELECT *, 
+        CASE 
+            WHEN position(lower($1) in lower(name)) = 1 THEN 3
+            WHEN position(' ' || lower($1) in lower(name)) > 0 THEN 2
+            ELSE 1
+        END as relevance_score
+    FROM user_data
+    WHERE lower(name) LIKE '%' || lower($1) || '%' AND uid <> $2
+    AND privacy NOT LIKE 'Private'
     AND uid NOT IN (
         SELECT "Sender" FROM friend WHERE "Receiver" = $2
         UNION
         SELECT "Receiver" FROM friend WHERE "Sender" = $2
     )
+    ORDER BY relevance_score DESC, name;
     `;
 
 export default async (req, res) => {
